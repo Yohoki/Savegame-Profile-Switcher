4928dcd0e4ad356e5305bce5fc6208ad13abb547
Yohoki <44595371+Yohoki@users.noreply.github.com>
2019-07-21 04:28:49 -0500

    Removed extra lines
    Compacted lines to make it easier when automation is started

--- GameConfig.bat
+++ GameConfig.bat
@@ -1,25 +1,20 @@
 ::Debug1 Profile
 if %Game%==Debug1 (
 set SaveDir="C:\DEBUG1"
-set SaveFldr=test
-)
+set SaveFldr=test )
 ::Debug2 profile
 if %Game%==Debug2 (
 set SaveDir="C:\DEBUG2"
-set SaveFldr=test
-)
+set SaveFldr=test )
 ::FarCry5 Profile
 if %Game%==FarCry5 (
 set SaveDir="C:\Program Files (x86)\Ubisoft\Ubisoft Game Launcher\savegames\"
-set SaveFldr=FarCry5
-)
+set SaveFldr=FarCry5 )
 ::Planet Coaster Profile
 if %Game%==PlanetCoaster (
 set SaveDir="C:\Users\%userprofile%\Saved Games\Frontier Developments\Planet Coaster\76561197993333907\"
-set SaveFldr=Saves
-)
+set SaveFldr=Saves )
 ::Assassin's Creed: Odyssey Profile
 if %Game%==ACOdyssey (
 set SaveDir="C:\Program Files (x86)\Ubisoft\Ubisoft Game Launcher\savegame\<UplayID>\5059"
-set SaveFldr=saves
-)
+set SaveFldr=saves )
